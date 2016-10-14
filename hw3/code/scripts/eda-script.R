4bc740f1a46279eb2b693147a3e01ed56fc7b28c
Jacqueline Liu <liu.jacqueline@berkeley.edu>
2016-10-14 13:48:48 -0700

    Hw3 Finished EDA script
    Formatted txt output better. Successfully saved scatterplot matrix as png.

--- hw3/code/scripts/eda-script.R
+++ hw3/code/scripts/eda-script.R
@@ -1,41 +1,51 @@
 library(ggplot2)
 
 # Set working directory to that of source file
-this.dir <- dirname(parent.frame(2)$ofile)
-setwd(paste0(this.dir, "/../.."))
+# this.dir <- dirname(parent.frame(2)$ofile)
+# setwd(paste0(this.dir, "/../.."))
 path <- getwd()
 
 ad.data <- read.csv(paste0(path, "/data/Advertising.csv"))
 ad.data$X <- NULL
 
-# Summary report and correlation matrix
-sink(file=paste0(path, "data/eda-output.txt"))
+# Summary report and correlation matrix as txt file
+cm <- cor(ad.data)
+sink(file=paste0(path, "/data/eda-output.txt"))
+cat("Data Summary\n")
 summary(ad.data)
-cor(ad.data)
+cat("\nCorrelation Matrix\n")
+cm
 sink()
 
+# Saving correlation matrix as object
+save(cm, file=paste0(path,"/data/correlation-matrix.RData"))
+
+# Scatterplot matrix
+png(filename=paste0(path, "/images/scatterplot-matrix.png"))
+pairs(ad.data)
+dev.off()
 
 # Generating histograms to help visualize distributions
 tv.hist <- ggplot(data=ad.data, aes(ad.data$TV)) + geom_histogram(alpha=0.7)
-ggsave(filename=paste0(path, "images/histogram-tv.png"), plot=tv.hist,
+ggsave(filename=paste0(path, "/images/histogram-tv.png"), plot=tv.hist,
        width=5, height=3)
-ggsave(filename=paste0(path, "images/histogram-tv.pdf"), plot=tv.hist,
+ggsave(filename=paste0(path, "/images/histogram-tv.pdf"), plot=tv.hist,
        width=5, height=3)
 
 radio.hist <- ggplot(data=ad.data, aes(ad.data$Radio)) + geom_histogram(alpha=0.7)
-ggsave(filename=paste0(path, "images/histogram-radio.png"), plot=radio.hist,
+ggsave(filename=paste0(path, "/images/histogram-radio.png"), plot=radio.hist,
        width=5, height=3)
-ggsave(filename=paste0(path, "images/histogram-radio.pdf"), plot=radio.hist,
+ggsave(filename=paste0(path, "/images/histogram-radio.pdf"), plot=radio.hist,
        width=5, height=3)
 
 news.hist <- ggplot(data=ad.data, aes(ad.data$Newspaper)) + geom_histogram(alpha=0.7)
-ggsave(filename=paste0(path, "images/histogram-news.png"), plot=news.hist,
+ggsave(filename=paste0(path, "/images/histogram-news.png"), plot=news.hist,
        width=5, height=3)
-ggsave(filename=paste0(path, "images/histogram-news.pdf"), plot=news.hist,
+ggsave(filename=paste0(path, "/images/histogram-news.pdf"), plot=news.hist,
        width=5, height=3)
 
 sales.hist <- ggplot(data=ad.data, aes(ad.data$Sales)) + geom_histogram(alpha=0.7)
-ggsave(filename=paste0(path, "images/histogram-sales.png"), plot=sales.hist,
+ggsave(filename=paste0(path, "/images/histogram-sales.png"), plot=sales.hist,
        width=5, height=3)
-ggsave(filename=paste0(path, "images/histogram-sales.pdf"), plot=sales.hist,
+ggsave(filename=paste0(path, "/images/histogram-sales.pdf"), plot=sales.hist,
        width=5, height=3)
