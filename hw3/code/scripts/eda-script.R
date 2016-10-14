a8dd87f50f07d94d7db5c434fb46ab846532a482
Jacqueline Liu <liu.jacqueline@berkeley.edu>
2016-10-14 14:31:53 -0700

    Hw3 Removed pdf outputs and associated code

--- hw3/code/scripts/eda-script.R
+++ hw3/code/scripts/eda-script.R
@@ -29,23 +29,15 @@ dev.off()
 tv.hist <- ggplot(data=ad.data, aes(ad.data$TV)) + geom_histogram(alpha=0.7)
 ggsave(filename=paste0(path, "/images/histogram-tv.png"), plot=tv.hist,
        width=5, height=3)
-ggsave(filename=paste0(path, "/images/histogram-tv.pdf"), plot=tv.hist,
-       width=5, height=3)
 
 radio.hist <- ggplot(data=ad.data, aes(ad.data$Radio)) + geom_histogram(alpha=0.7)
 ggsave(filename=paste0(path, "/images/histogram-radio.png"), plot=radio.hist,
-       width=5, height=3)
-ggsave(filename=paste0(path, "/images/histogram-radio.pdf"), plot=radio.hist,
        width=5, height=3)
 
 news.hist <- ggplot(data=ad.data, aes(ad.data$Newspaper)) + geom_histogram(alpha=0.7)
 ggsave(filename=paste0(path, "/images/histogram-news.png"), plot=news.hist,
        width=5, height=3)
-ggsave(filename=paste0(path, "/images/histogram-news.pdf"), plot=news.hist,
-       width=5, height=3)
 
 sales.hist <- ggplot(data=ad.data, aes(ad.data$Sales)) + geom_histogram(alpha=0.7)
 ggsave(filename=paste0(path, "/images/histogram-sales.png"), plot=sales.hist,
-       width=5, height=3)
-ggsave(filename=paste0(path, "/images/histogram-sales.pdf"), plot=sales.hist,
        width=5, height=3)
--- hw3/code/scripts/regression-script.R
+++ hw3/code/scripts/regression-script.R
@@ -1,6 +1,6 @@
 library(ggplot2)
  
-# Reading the data
+# Reading the dataset 
 path <-getwd()
 ad.data <- read.csv(paste0(path, "/data/Advertising.csv"))
 ad.data$X <- NULL
@@ -35,19 +35,13 @@ tv.reg <- ggplot(data=ad.data, aes(x=TV, y=Sales)) + geom_point(alpha=0.6, col="
   geom_smooth(method="lm", se=FALSE, col="darkblue")
 ggsave(filename=paste0(path, "/images/scatterplot-tv-sales.png"), plot=tv.reg,
        width=5, height=3)
-ggsave(filename=paste0(path, "/images/scatterplot-tv-sales.pdf"), plot=tv.reg,
-       width=5, height=3)
 
 radio.reg <- ggplot(data=ad.data, aes(x=Radio, y=Sales)) + geom_point(alpha=0.6, col="blue") + 
   geom_smooth(method="lm", se=FALSE, col="darkblue")
 ggsave(filename=paste0(path, "/images/scatterplot-radio-sales.png"), plot=radio.reg,
        width=5, height=3)
-ggsave(filename=paste0(path, "/images/scatterplot-radio-sales.pdf"), plot=radio.reg,
-       width=5, height=3)
 
 news.reg <- ggplot(data=ad.data, aes(x=Newspaper, y=Sales)) + geom_point(alpha=0.6, col="blue") + 
   geom_smooth(method="lm", se=FALSE, col="darkblue")
 ggsave(filename=paste0(path, "/images/scatterplot-news-sales.png"), plot=news.reg,
-       width=5, height=3)
-ggsave(filename=paste0(path, "/images/scatterplot-news-sales.pdf"), plot=news.reg,
        width=5, height=3)
